35c723f8b1bafb1fe40e8bb23fd6d5ac8aebfec9
nikolaitur <n.turovskyi@gmail.com>
2020-02-25 15:26:47 +0100

    Fix faq

--- app/views/install.vue
+++ app/views/install.vue
@@ -27,10 +27,10 @@
               <div class="card-body">
                 <div class="faq">
                   <div class="row">
-                    <strong><span class="badge badge-info">1.</span> I want to display form in different place. How to do it?</strong> <br/>
+                    <strong class="q"><span class="badge badge-info">1</span> I want to display form in different place. How to do it?</strong> <br/>
                     By default, Out of Stock Manager form is placed after the "add to cart" form. If you want it to display in different position of the website, use the code below: 
                     <pre>&lt;div class="mm-out-of-stock-manager_wrapper" data-id="&lt;&lt;formId&gt;&gt;">&lt;/div&gt;</pre>
-                      &lt;&lt;formId&gt;&gt;
+                    <pre><strong>&lt;&lt;formId&gt;&gt;</strong></pre>
                     </div>
                   </div>
               </div>
